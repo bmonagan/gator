8da3ceb4acf711067b46bcf5ee36d656a9b3e277
bmonagan <bernardmonagan@gmail.com>
2026-04-28 16:33:52 -0400

    fix: add handlerReset to command handlers and register it in main function

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { CommandsRegistry, runCommand } from "./com_reg";
 import { setUser,readConfig } from "./config";
 import { registerCommand } from "./com_reg";
-import { CommandHandler, handlerLogin, handlerRegister } from "./commandHandler";
+import { CommandHandler, handlerLogin, handlerRegister, handlerReset } from "./commandHandler";
 async function main() {
+  // Create registry and command handlers for commands
   const registry: CommandsRegistry = {};
   const login: CommandHandler = handlerLogin;
-  const register: CommandHandler = handlerRegister
+  const register: CommandHandler = handlerRegister;
+  const reset: CommandHandler = handlerReset;
   
+  // Register Comnmands 
   registerCommand(registry, "login", login);
   registerCommand(registry, "register",register);
+  registerCommand(registry, "reset", reset);
+
   let Fullargs:string[] = process.argv;
   let cmdName = Fullargs[2];
   let args = Fullargs.slice(3);

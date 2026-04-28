80c2f15f26b223df768ee29495ca520bfd0bb571
bmonagan <bernardmonagan@gmail.com>
2026-04-28 00:06:42 -0400

    feat: implement runCommand function for executing commands in the registry

--- src/com_reg.ts
+++ src/com_reg.ts
@@ -4,3 +4,12 @@ export type CommandsRegistry = Record<string, CommandHandler>;
 function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler) {
 	registry[cmdName] = handler;
 }
+
+function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]) {
+    if (!Object.hasOwn(registry,cmdName)){
+        throw new Error("Command name must be in the registry.");
+    }
+    else {
+        registry[cmdName](cmdName, ...args)
+    }
+}

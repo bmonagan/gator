ed41906d21275a8afffe8e4168d78886e22e4f94
bmonagan <bernardmonagan@gmail.com>
2026-04-27 23:58:31 -0400

    feat: implement handlerLogin function for user login handling

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,2 +1,12 @@
+import { setUser } from "./config";
+
 type CommandHandler = (cmdName: string, ...args: string[]) => void;
 
+function handlerLOgin(cmdName: string, ...args: string[]): void {
+    if (args.length === 0){
+        throw new Error("the login handler expects a single argument, the username");
+    }
+    let username = args[0];
+    setUser(username);
+    console.log("User name has been set.");
+}

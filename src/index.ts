a6ac36ece0426dc906fe13a7dc04eb831ca7e8cf
bmonagan <bernardmonagan@gmail.com>
2026-04-28 16:49:26 -0400

    feat: register handlerUsers command to list all users

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import { CommandsRegistry, runCommand } from "./com_reg";
 import { setUser,readConfig } from "./config";
 import { registerCommand } from "./com_reg";
-import { CommandHandler, handlerLogin, handlerRegister, handlerReset } from "./commandHandler";
+import { CommandHandler, handlerLogin, handlerRegister, handlerReset, handlerUsers } from "./commandHandler";
 async function main() {
   // Create registry and command handlers for commands
   const registry: CommandsRegistry = {};
   const login: CommandHandler = handlerLogin;
   const register: CommandHandler = handlerRegister;
   const reset: CommandHandler = handlerReset;
-  
+  const users: CommandHandler = handlerUsers;
+
   // Register Comnmands 
   registerCommand(registry, "login", login);
   registerCommand(registry, "register",register);
   registerCommand(registry, "reset", reset);
+  registerCommand(registry, "users", users);
 
   let Fullargs:string[] = process.argv;
   let cmdName = Fullargs[2];

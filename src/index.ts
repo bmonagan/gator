8ac516b555f01c4aa5f9ebf9c2766d115dc87a43
bmonagan <bernardmonagan@gmail.com>
2026-04-28 14:58:52 -0400

    feat: register command handler for user registration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { CommandsRegistry, runCommand } from "./com_reg";
 import { setUser,readConfig } from "./config";
 import { registerCommand } from "./com_reg";
-import { CommandHandler, handlerLogin } from "./commandHandler";
+import { CommandHandler, handlerLogin, handlerRegister } from "./commandHandler";
 async function main() {
   const registry: CommandsRegistry = {};
   const login: CommandHandler = handlerLogin;
+  const register: CommandHandler = handlerRegister
+  
   registerCommand(registry, "login", login);
+  registerCommand(registry, "register",register);
   let Fullargs:string[] = process.argv;
   let cmdName = Fullargs[2];
   let args = Fullargs.slice(3);

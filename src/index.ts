01c756e9024b3b87b4b6a36fce01ad575145dccb
bmonagan <bernardmonagan@gmail.com>
2026-04-27 22:21:21 -0400

    refactor: update main function to set user and log configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { setUser,readConfig } from "./config";
 function main() {
-  console.log("Hello, world!");
+  const name = "Benny";
+  setUser(name);
+  console.log(readConfig());
 }
 
 main();

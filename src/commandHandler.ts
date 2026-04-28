f9d7644341017a375ad6aa4f02ddde0b233a0c18
bmonagan <bernardmonagan@gmail.com>
2026-04-28 00:09:57 -0400

    feat: export handlerLogin function for external usage

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -2,7 +2,7 @@ import { setUser } from "./config";
 
 export type CommandHandler = (cmdName: string, ...args: string[]) => void;
 
-function handlerLOgin(cmdName: string, ...args: string[]): void {
+export function handlerLogin(cmdName: string, ...args: string[]): void {
     if (args.length === 0){
         throw new Error("the login handler expects a single argument, the username");
     }

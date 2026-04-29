a86924c771cb1f9411e8d178d3d936e148c0c79f
bmonagan <bernardmonagan@gmail.com>
2026-04-29 16:25:16 -0400

    fix: add handlerAggregate command handler registration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { CommandsRegistry, runCommand } from "./com_reg";
 import { setUser,readConfig } from "./config";
 import { registerCommand } from "./com_reg";
-import { CommandHandler, handlerLogin, handlerRegister, handlerReset, handlerUsers } from "./commandHandler";
+import { CommandHandler, handlerLogin, handlerRegister, handlerReset, handlerUsers, handlerAggregate } from "./commandHandler";
 async function main() {
   // Create registry and command handlers for commands
   const registry: CommandsRegistry = {};
@@ -9,12 +9,14 @@ async function main() {
   const register: CommandHandler = handlerRegister;
   const reset: CommandHandler = handlerReset;
   const users: CommandHandler = handlerUsers;
+  const agg: CommandHandler = handlerAggregate;
 
   // Register Comnmands 
   registerCommand(registry, "login", login);
   registerCommand(registry, "register",register);
   registerCommand(registry, "reset", reset);
   registerCommand(registry, "users", users);
+  registerCommand(registry,"agg",agg);
 
   let Fullargs:string[] = process.argv;
   let cmdName = Fullargs[2];

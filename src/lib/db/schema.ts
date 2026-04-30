5e2dfb1d09d1c24b059ba8b1526c2324b06fa79e
bmonagan <bernardmonagan@gmail.com>
2026-04-30 18:17:56 -0400

    feat: add posts table schema to database

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, timestamp, uuid, text, unique } from "drizzle-orm/pg-core";
+import { title } from "node:process";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom().notNull(),
@@ -33,3 +34,17 @@ export const feed_follows = pgTable("feed_follows", {
   feed_id: uuid("feed_id").notNull().references(() => feeds.id, { onDelete: "cascade" }),
   user_id: uuid("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
 }, (t) => [{user_feed: unique().on(t.user_id, t.feed_id)}]);
+
+export const posts = pgTable("posts", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  title: text("title").notNull(),
+  url: text("url").notNull(),
+  description: text("description").notNull(),
+  publishedAt: timestamp("published_at").notNull(),
+  feed_id: uuid("feed_id").notNull().references(() => feeds.id, { onDelete: "cascade" }),
+});

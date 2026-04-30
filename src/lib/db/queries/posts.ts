05db93ec31cd07109e579f101f65b7487104d5eb
bmonagan <bernardmonagan@gmail.com>
2026-04-30 18:27:18 -0400

    feat: add getPostsForUser function to retrieve posts for a specific user

--- src/lib/db/queries/posts.ts
+++ src/lib/db/queries/posts.ts
@@ -1,10 +1,30 @@
 import { db } from "..";
 import { eq } from "drizzle-orm";
 import { posts, feeds, users } from "../schema";
+
 export async function createPost(title: string, url: string, description: string, publishedAt: Date, feedId: string) {
     const [result] = await db
         .insert(posts)
         .values({ title, url, description, publishedAt, feed_id: feedId })
         .returning();
+    return result;
+}
+
+export async function getPostsForUser(userId: string, limit: number = 20) {
+    const result = await db
+        .select({
+            title: posts.title,
+            url: posts.url,
+            description: posts.description,
+            publishedAt: posts.publishedAt,
+            feedName: feeds.name,
+            userName: users.name,
+        })
+        .from(posts)
+        .innerJoin(feeds, eq(posts.feed_id, feeds.id))
+        .innerJoin(users, eq(feeds.user_id, users.id))
+        .where(eq(users.id, userId))
+        .orderBy(posts.publishedAt.desc())
+        .limit(limit);
     return result;
 }
